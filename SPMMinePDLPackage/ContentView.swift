8d3d4d4c86f0be97a069a268cbe1f935c3b43f32
DedeepyaSallaPhoton <dedeepya.salla@photon.com>
2023-09-25 12:20:02 -0400

    Added changes

--- SPMMinePDLPackage/ContentView.swift
+++ SPMMinePDLPackage/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world jhkjjlk!")
+            Text("Hello, world jhkjjlk hkjhk!")
         }
         .padding()
     }
