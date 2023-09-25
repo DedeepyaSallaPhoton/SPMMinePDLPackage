24e012c4f891fd698935919e7a380daa9a30a648
DedeepyaSallaPhoton <dedeepya.salla@photon.com>
2023-09-25 12:14:24 -0400

    Initial Commit

---
+++ SPMMinePDLPackage/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SPMMinePDLPackage
+//
+//  Created by Dedeepya Reddy Salla on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world jhkjjlk!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SPMMinePDLPackage/SPMMinePDLPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMMinePDLPackageApp.swift
+//  SPMMinePDLPackage
+//
+//  Created by Dedeepya Reddy Salla on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SPMMinePDLPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
